9e98dd58fcc0785882ba7925d0000f01fd788252
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-09-29 14:52:44 -0400

    Fixed broken test case

--- test/models/line_item_date_test.rb
+++ test/models/line_item_date_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class LineItemDateTest < ActiveSupport::TestCase
   test "#previous_date returns the quote's previous date when it exitsts" do
-    assert_equal line_item_dates(:one), line_item_dates(:next_week).previous_date
+    assert_equal line_item_dates(:two), line_item_dates(:next_week).previous_date
   end
 
   test "#previous_date returns nil when the quote has no previous date" do
