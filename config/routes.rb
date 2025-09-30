9f6ec22c92c8104023a67b298e44ec237271a5cb
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-09-30 12:16:27 -0400

    Updated the routes for line item

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root "pages#home"
   devise_for :users
   resources :quotes do
-    resources :line_item_dates, except: [ :index, :show ]
+    resources :line_item_dates, except: [ :index, :show ] do
+      resources :line_item, except: [ :index, :show ]
+    end
   end
 end
