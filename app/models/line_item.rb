ac27b484d6bb1708f1b9c026f05d2513209161d7
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-09-30 13:17:57 -0400

    Fixed type in LineItem model

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -3,5 +3,5 @@ class LineItem < ApplicationRecord
 
   validates :name, presence: true
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :unit_price, presence: true, numericality: { greater_than: 0 }
 end
