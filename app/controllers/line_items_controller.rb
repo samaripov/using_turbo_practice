7e5b1845e9dda14fa1b5deae3e2122af23509674
Aripov-Sirojiddin <169399701+Aripov-Sirojiddin@users.noreply.github.com>
2025-09-30 14:20:16 -0400

    Added the edit functionality to line items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,6 +1,7 @@
 class LineItemsController < ApplicationController
   before_action :set_quote
   before_action :set_line_item_date
+  before_action :set_line_item, only: %i[ edit update ]
   def new
     @line_item = @line_item_date.line_items.build
   end
@@ -11,7 +12,18 @@ class LineItemsController < ApplicationController
     if @line_item.save
       redirect_to @quote, notice: "Item created"
     else
-      render :new, status: unprocessable_content
+      render :new, status: :unprocessable_content
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @line_item.update(line_item_params)
+      redirect_to @quote, notice: "Item updated"
+    else
+      render :edit, status: :unprocessable_content
     end
   end
 
@@ -26,5 +38,9 @@ class LineItemsController < ApplicationController
 
     def set_line_item_date
       @line_item_date = @quote.line_item_dates.find(params[:line_item_date_id])
+    end
+
+    def set_line_item
+      @line_item = @line_item_date.line_items.find(params[:id])
     end
 end
